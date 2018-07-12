b818a3368c8a73089d9e28402608b498b5ce9dbc
i2bskn <i2bskn@gmail.com>
2018-07-12 18:24:56 +0900

    Load environment yaml when using and bump version

--- lib/app_builder/environment.rb
+++ lib/app_builder/environment.rb
@@ -1,10 +1,11 @@
 module AppBuilder
   class Environment
-    attr_accessor :name, :source
+    attr_accessor :name
+    attr_reader :source_path
 
-    def initialize(name, source)
-      @name   = name
-      @source = source
+    def initialize(name, source_path)
+      @name        = name
+      @source_path = source_path
     end
 
     def create_file(template_path, output_path)
@@ -18,6 +19,10 @@ module AppBuilder
       Class.new.tap { |klass|
         klass.define_method(:env) { env }
       }.instance_eval { binding }
+    end
+
+    def source
+      @source ||= YAML.load_file(source_path)
     end
   end
 end
