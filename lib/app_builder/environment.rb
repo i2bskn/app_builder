238f03b5386930d32f56760022ef819c087170a2
i2bskn <i2bskn@gmail.com>
2018-07-25 14:23:14 +0900

    Load environment from real path

--- lib/app_builder/environment.rb
+++ lib/app_builder/environment.rb
@@ -3,11 +3,12 @@ module AppBuilder
     attr_accessor :name
     attr_reader :repo_path, :branch, :source_path
 
-    def initialize(source_path, name: nil, repo_path: nil, branch: nil)
-      @name        = name || ENV.fetch("APP_ENV", "default")
-      @repo_path   = repo_path || `git rev-parse --show-toplevel`.chomp
-      @branch      = branch || ENV.fetch("TARGET_BRANCH", `git symbolic-ref --short HEAD`.chomp)
+    def initialize(source_path, options = {})
+      @name        = options[:name] || ENV.fetch("APP_ENV", "default")
+      @repo_path   = options[:repo_path] || `git rev-parse --show-toplevel`.chomp
+      @branch      = options[:branch] || ENV.fetch("TARGET_BRANCH", `git symbolic-ref --short HEAD`.chomp)
       @source_path = source_path
+      @from_branch = options.fetch(:from_branch, true)
     end
 
     def create_file(template_path, output_path)
@@ -33,13 +34,23 @@ module AppBuilder
     alias :hash :to_hash
 
     def source
-      @source ||= YAML.load(
-        ERB.new(
-          Dir.chdir(repo_path) {
-            `git show #{branch}:#{source_path}`
-          }
-        ).result(binding)
-      )
+      @source ||= load_source
     end
+
+    private
+
+      def load_source
+        if from_branch
+          YAML.load(
+            ERB.new(
+              Dir.chdir(repo_path) {
+                `git show #{branch}:#{source_path}`
+              }
+            ).result(binding)
+          )
+        else
+          YAML.load_file(source_path)
+        end
+      end
   end
 end
