e7e83b39483b2717e3ffd4057e71d3a0d8bdbbff
i2bskn <i2bskn@gmail.com>
2018-07-30 15:27:05 +0900

    Improve env access in templates.

--- lib/app_builder/environment.rb
+++ lib/app_builder/environment.rb
@@ -1,27 +1,20 @@
 module AppBuilder
   class Environment
     attr_accessor :name
-    attr_reader :repo_path, :branch, :source_path
+    attr_reader :repo_path, :branch, :source_path, :source_type
 
     def initialize(source_path, options = {})
       @name        = options[:name] || ENV.fetch("APP_ENV", "default")
       @repo_path   = options[:repo_path] || `git rev-parse --show-toplevel`.chomp
       @branch      = options[:branch] || ENV.fetch("TARGET_BRANCH", `git symbolic-ref --short HEAD`.chomp)
       @source_path = source_path
-      @from_branch = options.fetch(:from_branch, true)
+      @source_type = options.fetch(:source_type, :git) # :git (git show branch:path) or :path (File.read(path))
     end
 
     def create_file(template_path, output_path)
       File.open(output_path, "w") do |f|
-        f.write(ERB.new(File.read(template_path)).result(template_binding))
+        f.write(ERB.new(File.read(template_path)).result(binding))
       end
-    end
-
-    def template_binding
-      env = source.fetch(name.to_s)
-      Class.new.tap { |klass|
-        klass.define_method(:env) { env }
-      }.instance_eval { binding }
     end
 
     def [](key)
@@ -29,28 +22,37 @@ module AppBuilder
     end
 
     def to_hash
-      source.fetch(name.to_s)
+      raw_source.fetch(name.to_s)
     end
     alias :hash :to_hash
 
-    def source
+    def raw_source
       @source ||= load_source
     end
 
     private
 
       def load_source
-        if from_branch
-          YAML.load(
-            ERB.new(
-              Dir.chdir(repo_path) {
-                `git show #{branch}:#{source_path}`
-              }
-            ).result(binding)
-          )
+        case source_type
+        when :git
+          raw_src = Dir.chdir(repo_path) { `git show #{branch}:#{source_path}` }
+        when :path
+          raw_src = File.read(source_path)
         else
-          YAML.load_file(source_path)
+          raise "Unknown source_type: #{source_type}"
         end
+
+        YAML.load(ERB.new(raw_src).result(binding))
+      end
+
+      def method_missing(name, *args, &block)
+        super unless hash.has_key?(name.to_s)
+
+        hash.fetch(name.to_s)
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        hash.has_key?(name.to_s)
       end
   end
 end
